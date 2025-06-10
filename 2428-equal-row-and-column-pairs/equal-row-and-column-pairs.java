761a4f7b12c7f3a8211ec782c0987de314700fa0
navabhagath <navabhagath.2004@gmail.com>
2025-06-10 22:55:26 +0530

    Time: 70 ms (17.61%) | Memory: 52.4 MB (31.58%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
@@ -1,38 +1,19 @@
 class Solution {
     public int equalPairs(int[][] grid) {
         int n = grid.length;
-        int p=31;
-        List<String>row=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            StringBuilder sb=new StringBuilder();
-            for(int j=0;j<n;j++){
-                sb.append(grid[i][j]+"");
-                if(j<n-1){
-                    sb.append("-");
-                }
-            }
-            row.add(sb.toString());
-        }
-        List<String>col=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            StringBuilder sb=new StringBuilder();
-            for(int j=0;j<n;j++){
-                sb.append(grid[j][i]+"");
-                if(j<n-1){
-                    sb.append("-");
-                }
-            }
-            col.add(sb.toString());
-        }
         int ans=0;
         for(int i=0;i<n;i++){
+            int[] row = grid[i];
             for(int j=0;j<n;j++){
-                if(row.get(i).equals(col.get(j))){
+                int[] col = new int[n];
+                for(int k=0;k<n;k++){
+                    col[k]=grid[k][j];
+                }
+                if(Arrays.equals(row,col)){
                     ans++;
                 }
             }
         }
         return ans;
-        
     }
 }
