ff26ac4031078ef045f0c88434d17609117ea8c3
navabhagath <navabhagath.2004@gmail.com>
2025-06-16 20:08:21 +0530

    Time: 5 ms (29.94%) | Memory: 42.3 MB (33.90%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.java
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.java
@@ -1,7 +1,8 @@
 class Solution {
     public int maximumDifference(int[] nums) {
         int n = nums.length;
-        int ans=Integer.MIN_VALUE;
+        // int ans=Integer.MIN_VALUE;
+        int ans=-1;
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 if(nums[j] > nums[i]){
@@ -9,9 +10,9 @@ class Solution {
                 }
             }
         }
-        if(ans == Integer.MIN_VALUE){
-            return -1;
-        }
+        // if(ans == Integer.MIN_VALUE){
+        //     return -1;
+        // }
         return ans;
     }
 }
