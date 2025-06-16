ed633f80c485a684aef008818499a4bef60ab4a6
navabhagath <navabhagath.2004@gmail.com>
2025-06-16 22:45:46 +0530

    Time: 57 ms (11.93%) | Memory: 57.7 MB (6.42%) - LeetSync

--- 927-sum-of-subsequence-widths/sum-of-subsequence-widths.java
+++ 927-sum-of-subsequence-widths/sum-of-subsequence-widths.java
@@ -4,14 +4,11 @@ class Solution {
         int n = nums.length;
         long ans = 0;
         Arrays.sort(nums);
-        long []pow = new long[n];
+        long []pow = new long[100001];
         pow[0]=1;
-        for(int i=1;i<n;i++){
+        for(int i=1;i<100001;i++){
             pow[i] = (pow[i-1]*2)%MOD;
         }
-        // for(int i=0;i<n;i++){
-        //     pow[i] = (1<<i)%MOD;
-        // }
         for(int i=0;i<n;i++){
             ans = (ans + (nums[i] - nums[n-i-1]) * pow[i]) % MOD ;
         }
