014f9cde2405baccf5a749147f25885b4ac579bf
navabhagath <navabhagath.2004@gmail.com>
2025-06-18 18:12:59 +0530

    Time: 13 ms (28.69%) | Memory: 46.6 MB (62.81%) - LeetSync

--- 561-array-partition/array-partition.java
+++ 561-array-partition/array-partition.java
@@ -4,8 +4,7 @@ class Solution {
         int n = nums.length;
         int sum = 0;
         for(int i=0;i<n;i++){
-            sum += Math.min(nums[i],nums[i+1]);
-            i++;
+            sum += nums[i++];
         }
         return sum;
     }
