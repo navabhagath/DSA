4e99f624f9a7ef603f4a0c52d0c74c2f144cc302
navabhagath <navabhagath.2004@gmail.com>
2025-06-18 18:08:27 +0530

    Time: 40 ms (14.57%) | Memory: 45.8 MB (14.35%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int jump(int[] nums) {
+        int n = nums.length;
+        int []dp=new int[n];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[n-1]=0;
+        for(int i=n-2;i>=0;i--){
+            for(int j=i+1;j<n && j<=i+nums[i];j++){
+                if(dp[j]!=Integer.MAX_VALUE){
+                    dp[i] = Math.min(dp[i],dp[j]+1);
+                }
+            }
+        }
+        return dp[0] == Integer.MAX_VALUE ? -1 : dp[0];
+    }
+}
