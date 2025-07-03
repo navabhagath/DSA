60fa3fe8ab6f9051029e2f3bcc7679c8dfe621fb
navabhagath <navabhagath.2004@gmail.com>
2025-07-03 13:09:06 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (94.55%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.java
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.java
@@ -1,17 +1,27 @@
 class Solution {
-    public char kthCharacter(int k) {
-        
-        StringBuilder sb = new StringBuilder();
-        sb.append("a");
-        while(sb.length() < 500){
-            StringBuilder temp = new StringBuilder();
-            for(int i=0;i<sb.length();i++){
-                char c = sb.charAt(i);
-                temp.append( (char)  (((c-'a'+1)%26) + 'a')      );
-            }
-            sb.append(temp);
+    public int setBits(int n){
+        int ans=0;
+        while(n!=0){
+            if( (n&1)  == 1){
+                ans++;
+            } 
+            n = n>>1;
         }
-        System.out.println(sb.toString());
-        return sb.charAt(k-1);
+        return ans;
+    }
+    public char kthCharacter(int k) {
+        // StringBuilder sb = new StringBuilder();
+        // sb.append("a");
+        // while(sb.length() < 500){
+        //     StringBuilder temp = new StringBuilder();
+        //     for(int i=0;i<sb.length();i++){
+        //         char c = sb.charAt(i);
+        //         temp.append( (char)  (((c-'a'+1)%26) + 'a')      );
+        //     }
+        //     sb.append(temp);
+        // }
+        // System.out.println(sb.toString());
+        int ans = setBits(k-1);
+        return  (char) ('a' + ans);
     }
 }
