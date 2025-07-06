5767bd003874a372300d188e2f022c35cbe61135
navabhagath <navabhagath.2004@gmail.com>
2025-07-06 14:54:02 +0530

    Time: 7 ms (6.84%) | Memory: 42.1 MB (20.91%) - LeetSync

---
+++ 2668-form-smallest-number-from-two-digit-arrays/form-smallest-number-from-two-digit-arrays.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minNumber(int[] nums1, int[] nums2) {
+        TreeSet<Integer>hs1 = new TreeSet<>();
+        TreeSet<Integer>hs2 = new TreeSet<>();
+        int min1 = Arrays.stream(nums1).min().getAsInt();
+        int min2 = Arrays.stream(nums2).min().getAsInt();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        for(int num : nums1){
+            hs1.add(num);
+        }
+        for(int num : nums2){
+            hs2.add(num);
+        }
+        for(int i=0;i<nums1.length;i++){
+            if(hs2.contains(nums1[i])){
+                return nums1[i];
+            }
+        }
+        if(min1 > min2){
+            return min2*10 + min1;
+        }
+        return min1*10 + min2;
+    }
+}
