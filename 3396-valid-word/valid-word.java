a3e47b59846098398791bef4316eb2ccf10b5fd3
navabhagath <navabhagath.2004@gmail.com>
2025-07-15 20:53:17 +0530

    Time: 1 ms (99.12%) | Memory: 41.8 MB (97.13%) - LeetSync

---
+++ 3396-valid-word/valid-word.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isVowel(char ch){
+        return ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U';
+    }
+    public boolean isValid(String word) {
+        int n = word.length();
+        if(n<3) return false;
+        boolean c = false;
+        boolean v = false;
+        boolean num = false;
+        boolean sym = false;
+        for(char ch : word.toCharArray()){
+            if(ch >='0' && ch<='9'){
+                num = true;
+            }else if(isVowel(ch)){
+                v = true;
+            }else if(!isVowel(ch) && (ch>='a' && ch<='z') || (ch>='A' && ch<='Z')){
+                c = true;
+            }else{
+                return false;
+            }
+            
+        }
+        return v && c ;
+    }
+}
