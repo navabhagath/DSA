be5a1c43c80dac292f82825c945baa2ad8b0e3ee
navabhagath <navabhagath.2004@gmail.com>
2025-08-02 16:09:35 +0530

    Time: 43 ms (81.69%) | Memory: 68 MB (14.08%) - LeetSync

---
+++ 2689-rearranging-fruits/rearranging-fruits.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long minCost(int[] basket1, int[] basket2) {
+        int n = basket1.length;
+        Map<Integer,Integer>mp = new HashMap<>();
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            mp.put(basket1[i],mp.getOrDefault(basket1[i],0)+1);
+            mp.put(basket2[i],mp.getOrDefault(basket2[i],0)-1);
+            min = Math.min(min,Math.min(basket1[i],basket2[i]));
+        }
+        List<Integer>temp = new ArrayList<>();
+        for(Integer key : mp.keySet()){
+            int cnt = mp.get(key);
+            if(cnt%2!=0) return -1;
+            for(int i=0;i<Math.abs(cnt/2);i++){
+                temp.add(key);
+            }
+        }
+        Collections.sort(temp);
+        long ans = 0;
+        for(int i=0;i<temp.size()/2;i++){
+            ans+= Math.min(min*2,temp.get(i));
+        }
+        return ans;
+    }
+}
