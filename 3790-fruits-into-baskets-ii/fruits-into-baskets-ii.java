d330dc343d3acb504eae51e6dd2617f72482f76b
navabhagath <navabhagath.2004@gmail.com>
2025-08-05 11:02:37 +0530

    Time: 1 ms (100.00%) | Memory: 44.7 MB (22.68%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
@@ -4,7 +4,7 @@ class Solution {
         int cnt = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(f[i] <= b[j] && b[j]!=0){
+                if(f[i] <= b[j]){
                     b[j] = 0;
                     cnt++;
                     break;
