811d36bac732afdabb1e18497b050e46876b06c7
navabhagath <navabhagath.2004@gmail.com>
2025-08-15 13:59:58 +0530

    Time: 3 ms (17.90%) | Memory: 44.7 MB (96.35%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,45 +1,22 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]>inter = new ArrayList<>(Arrays.asList(intervals));
+        inter.add(newInterval);
+        int n = inter.size();
+        Collections.sort(inter, (a,b) -> Integer.compare(a[0],b[0]));
         List<int[]>res = new ArrayList<>();
-        for(int []x : intervals){
-            res.add(x);
-        }
-        int n = intervals.length;
-        res.add(newInterval);
-        Collections.sort(res, (a,b) -> Integer.compare(a[0] , b[0]) );
-        List<Pair>ans = new ArrayList<>();
-        int []current = res.get(0);
-        ans.add(new Pair(current[0] , current[1]));
-        for(int i=1;i<n+1;i++){
-            int curf = res.get(i)[0];
-            int curs = res.get(i)[1];
-            int f = ans.get(ans.size()-1).f;
-            int s = ans.get(ans.size()-1).s;
-            if(curf <= s){
-                ans.remove(ans.size()-1);
-                ans.add(new Pair(f,Math.max(s,curs)));
+        int []curr = inter.get(0);
+        for(int i=0;i<n;i++){
+            int []interval = inter.get(i);
+            if(interval[0] <= curr[1]){
+                curr[1] = Math.max(curr[1] , interval[1]);
             }else{
-                ans.add(new Pair(curf,curs));
+                res.add(curr);
+                curr = interval;
             }
         }
-        for(Pair z : ans){
-            System.out.println(z.f +" "+z.s);
-        }
-        int [][]result = new int[ans.size()][2];
-        int ind = 0;
-        for(Pair z : ans){
-            result[ind][0] = z.f;
-            result[ind][1] = z.s;
-            ind++;
-        }
-        return result;
-    }
-}
-class Pair{
-    int f;
-    int s;
-    Pair(int f,int s){
-        this.f = f;
-        this.s = s;
+        res.add(curr);
+        return res.toArray(new int[res.size()][]);
+        
     }
 }
