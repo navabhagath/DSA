1fbf9571f1217e67b7a6d99d48316deb5893ac3c
navabhagath <navabhagath.2004@gmail.com>
2025-08-15 13:17:31 +0530

    Time: 8 ms (85.64%) | Memory: 46.7 MB (55.70%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,38 +1,18 @@
 class Solution {
-
     public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals,(a,b)-> Integer.compare(a[0],b[0]));
         int n = intervals.length;
-        Arrays.sort(intervals,(a,b) -> Integer.compare(a[0],b[0]));
-        List<Pair>res = new ArrayList<>();
-        res.add(new Pair(intervals[0][0] , intervals[0][1]));
-        for(int i=1;i<n;i++){
-            Pair x = res.get(res.size()-1);
-            int f = x.first;
-            int s = x.second;
-            int curf = intervals[i][0];
-            int curs = intervals[i][1];
-            if(curf <= s){
-                res.remove(res.get(res.size()-1));
-                res.add(new Pair(f,Math.max(s,curs)));
+        List<int []>res = new ArrayList<>();
+        int []temp = intervals[0];
+        res.add(temp);
+        for(int []interval : intervals){
+            if(interval[0] <= temp[1]){
+                temp[1] = Math.max(interval[1] , temp[1]);
             }else{
-                res.add(new Pair(curf,curs));
+                temp = interval;
+                res.add(temp);
             }
         }
-        int [][]ans = new int[res.size()][2];
-        int idx = 0;
-        for(Pair y : res){
-            ans[idx][0] = y.first;
-            ans[idx][1] = y.second;
-            idx++;
-        } 
-        return ans;
-    }
-}
-class Pair{
-    int first;
-    int second;
-    Pair(int first,int second){
-        this.first = first;
-        this.second = second;
+        return res.toArray(new int[res.size()][]);
     }
 }
