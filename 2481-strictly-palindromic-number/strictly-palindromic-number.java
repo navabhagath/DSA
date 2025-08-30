83382117cc02520d41adff79d207b85884b39afc
navabhagath <navabhagath.2004@gmail.com>
2025-08-30 15:19:55 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (54.89%) - LeetSync

--- 2481-strictly-palindromic-number/strictly-palindromic-number.java
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.java
@@ -1,25 +1,5 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-        for(int i=2;i<=n-2;i++){
-            String bin = Integer.toString(n,i);
-            if(!helper(bin)){
-                return false;
-            }
-        }
-        return true;
-    }
-    public boolean helper(String str){
-        int n = str.length();
-        int i=0;
-        int j=n-1;
-        while(i < j){
-            if(str.charAt(i) == str.charAt(j)){
-                i++;
-                j--;
-            }else{
-                return false;
-            }
-        }
-        return true;
+        return false;
     }
 }
