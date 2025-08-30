92bca0530ed41709befc6e60bd24c823a12f1268
navabhagath <navabhagath.2004@gmail.com>
2025-08-30 11:03:13 +0530

    Time: 3 ms (52.39%) | Memory: 44.5 MB (66.47%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,22 +1,36 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-                if(!isValid(i,j,board)){
-                    return false;
+        for(int i=0;i<9;i+=3){
+            for(int j=0;j<9;j+=3){
+                Set<Character>hs = new HashSet<>();
+                for(int x=0;x<3;x++){
+                    for(int y=0;y<3;y++){
+                        char ch = board[i+x][j+y];
+                        if(ch == '.') continue;
+                        if(hs.contains(ch)){
+                            return false;
+                        }else{
+                            hs.add(ch);
+                        }
+                    }
                 }
             }
         }
-        return true;
-    }
-    public boolean isValid(int row,int col,char[][] board){
-        if(board[row][col] == '.') return true;
-
         for(int i=0;i<9;i++){
-            if(col!=i && board[row][i] == board[row][col]) return false;
-            if(row!=i && board[i][col] == board[row][col]) return false;
-            if((3*(row/3)+(i/3)) != row &&  (3*(col/3)+(i%3)) != col && 
-            board [(3*(row/3)+(i/3))] [(3*(col/3)+(i%3))] == board[row][col]) return false;
+            Set<Character>hs = new HashSet<>();
+            for(int j=0;j<9;j++){
+                if(board[i][j] == '.') continue;
+                if(hs.contains(board[i][j])) return false;
+                else hs.add(board[i][j]);
+            }
+        }
+        for(int i=0;i<9;i++){
+            Set<Character>hs = new HashSet<>();
+            for(int j=0;j<9;j++){
+                if(board[j][i] == '.') continue;
+                if(hs.contains(board[j][i])) return false;
+                else hs.add(board[j][i]);
+            }
         }
         return true;
     }
