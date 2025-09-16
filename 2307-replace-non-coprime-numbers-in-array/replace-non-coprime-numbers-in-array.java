19476fb557f1adaaf843fd602a3075228fcacb7e
navabhagath <navabhagath.2004@gmail.com>
2025-09-16 12:21:59 +0530

    Time: 1788 ms (5.15%) | Memory: 60.4 MB (89.69%) - LeetSync

--- 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.java
+++ 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.java
@@ -4,16 +4,19 @@ class Solution {
         int n = nums.length;
         for(int i=0;i<n;i++){
             st.push(nums[i]);
-            while(st.size() > 1){
-                int num1 = st.get(st.size()-1);
-                int num2 = st.get(st.size()-2);
-                int g = gcd(num1,num2);
-                if(g==1) break;
-                st.pop();
-                st.pop();
-                long lcm1 =(1L * num1 * num2) / g;
-                st.push((int)lcm1);
-            } 
+           while(st.size() > 1){
+                int num1 = st.get(st.size()-1); 
+                int num2 = st.get(st.size()-2); 
+                if(gcd(num1,num2) > 1){ 
+                    long lcm1 = ( 1L * num1 * num2 ) / gcd(num1,num2); 
+                    st.pop(); 
+                    st.pop(); 
+                    st.push((int)lcm1); 
+                }else{
+                    break;
+                }
+            }
+
         }
         List<Integer>ans = new ArrayList<>();
         System.out.println(st);
