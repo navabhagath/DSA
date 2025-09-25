560a9f33c83646fc912cb1b66462bef712c9b42f
navabhagath <navabhagath.2004@gmail.com>
2025-09-25 18:50:04 +0530

    Time: 1 ms (99.85%) | Memory: 45.5 MB (53.96%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,24 +1,23 @@
 class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
-        int [][]dp = new int[n][n];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],Integer.MAX_VALUE);
+    public int minimumTotal(List<List<Integer>> t) {
+        int n = t.size();
+        int [][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], Integer.MAX_VALUE); // sentinel
         }
-        dp[0][0] = triangle.get(0).get(0);
-        for(int i=1;i<n;i++){
-            for(int j=0;j<=i;j++){
-                if(j == 0){
-                    dp[i][j] = triangle.get(i).get(j) + dp[i-1][j];
-                }else{
-                    dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i-1][j-1] , dp[i-1][j]);
-                }
-            }
+        return solve(0, 0, t, dp);
+    }
+
+    private int solve(int i, int j, List<List<Integer>> t, int[][] dp) {
+        int n = t.size();
+        if (i == n - 1) {
+            return dp[i][j] = t.get(i).get(j);
         }
-        int ans = Integer.MAX_VALUE;
-        for(int j=0;j<n;j++){
-            ans = Math.min(ans , dp[n-1][j]);
-        }
-        return ans;
+        if (dp[i][j] != Integer.MAX_VALUE) return dp[i][j];
+
+        int a = solve(i + 1, j, t, dp);
+        int b = solve(i + 1, j + 1, t, dp);
+
+        return dp[i][j] = t.get(i).get(j) + Math.min(a, b);
     }
 }
